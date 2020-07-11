e672fc7dc09a862ec0afbcd0fec63f074542ad1b
Abhinav-Reddy-k <abhinavreddykanala@gmail.com>
2020-07-11 13:52:43 -0600

    Created projects reducer and combined with bugs.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import { bugAdded, bugRemoved, bugResolved } from "./store/bugs";
 import getStore from "./store/configureStore";
+import { projectAdded } from "./store/projects";
 
 const store = getStore();
 
@@ -11,3 +12,5 @@ store.dispatch(bugAdded({ description: "Bug1" }));
 store.dispatch(bugAdded({ description: "Bug2" }));
 store.dispatch(bugRemoved({ id: 1 }));
 store.dispatch(bugResolved({ id: 2 }));
+
+store.dispatch(projectAdded({ name: "Abhinav redux project" }));
--- src/store/bugs.js
+++ src/store/bugs.js
@@ -1,31 +1,30 @@
-import { createAction, createReducer } from "@reduxjs/toolkit";
+import { createSlice } from "@reduxjs/toolkit";
 
-// Actions
-
-export const bugAdded = createAction("bugAdded");
-
-export const bugRemoved = createAction("bugRemoved");
-
-export const bugResolved = createAction("bugResolved");
-
-// Reducers
 let lastId = 0;
 
-export default createReducer([], {
-  [bugAdded.type]: (bugs, action) => {
-    bugs.push({
-      id: ++lastId,
-      discription: action.payload.discription,
-      resolved: false,
-    });
-  },
+const slice = createSlice({
+  name: "bugs",
+  initialState: [],
 
-  [bugRemoved.type]: (bugs, action) => {
-    bugs.filter((bug) => bug.id !== action.payload.id);
-  },
+  reducers: {
+    bugAdded: (bugs, action) => {
+      bugs.push({
+        id: ++lastId,
+        discription: action.payload.discription,
+        resolved: false,
+      });
+    },
 
-  [bugResolved.type]: (bugs, action) => {
-    const index = bugs.findIndex((bug) => bug.id === action.payload.id);
-    bugs[index].resolved = true;
+    bugResolved: (bugs, action) => {
+      const index = bugs.findIndex((bug) => bug.id === action.payload.id);
+      bugs[index].resolved = true;
+    },
+
+    bugRemoved: (bugs, action) => {
+      bugs.filter((bug) => bug.id !== action.payload.id);
+    },
   },
 });
+
+export const { bugAdded, bugRemoved, bugResolved } = slice.actions;
+export default slice.reducer;
